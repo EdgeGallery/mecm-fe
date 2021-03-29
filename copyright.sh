bbff337d06a8a3cfce30e7e7194c4039b02d6dbf
WensonYang <yyang2_11@163.com>
2021-03-29 09:58:04 +0800

    copyright更新

---
+++ copyright.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for filename in `git ls-tree -r --name-only HEAD`
+do
+    addYear=$(git log --pretty=format:"%ad %H %an" -- $filename | tail -1 | awk '{print $5}')
+    modYear=$(git log -1 --pretty=format:"%ad %H %an" -- $filename | awk '{print $5}')
+    if [ $modYear -gt $addYear ]; then
+        sed -i "s/Copyright \([0-9]\{4\}\(-[0-9]\{4\}\)*\)/Copyright $addYear-$modYear/" $filename
+    elif [ $modYear -eq $addYear ]; then
+        sed -i "s/Copyright \([0-9]\{4\}\(-[0-9]\{4\}\)*\)/Copyright $addYear/" $filename
+    fi
+done
