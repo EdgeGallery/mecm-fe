aadbe3fdbe7b289b901e77281f0fc26bc9e6585e
zhaolongfei_123 <951084082@qq.com>
2021-12-06 16:19:16 +0800

    I4G4W1【mecm-fe】MECM中心侧支持轻量级云管功能-同步mecm

---
+++ src/pages/resource/form/NetworkForm.vue
@@ -0,0 +1,223 @@
+<!--
+  - Copyright 2021 Huawei Technologies Co., Ltd.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+<template>
+  <div class="network-form">
+    <el-dialog
+      :visible.sync="dialogVisible"
+      width="40%"
+      :before-close="handleClose"
+      :close-on-click-modal="false"
+      class="default_dialog"
+    >
+      <div
+        slot="title"
+        class="el-dialog__title"
+      >
+        <em class="title_icon" />{{ $t('resourceMgr.createNetwork') }}
+      </div>
+      <el-form
+        :model="createNetworkForm"
+        ref="form"
+        :rules="rules"
+        label-position="right"
+        :label-width="language==='cn'? '100px': '130px'"
+      >
+        <el-form-item
+          :label="$t('resourceMgr.networkName')"
+          prop="networkName"
+          class="w100"
+        >
+          <el-input
+            size="small"
+            v-model="createNetworkForm.networkName"
+          />
+        </el-form-item>
+        <el-form-item
+          :label="$t('resourceMgr.subnetName')"
+          prop="subnetName"
+          class="w100"
+        >
+          <el-input
+            size="small"
+            v-model="createNetworkForm.subnetName"
+          />
+        </el-form-item>
+        <el-form-item
+          :label="$t('resourceMgr.networkAddr')"
+          prop="networkAddr"
+          class="w100"
+        >
+          <el-input
+            size="small"
+            v-model="createNetworkForm.networkAddr"
+          />
+        </el-form-item>
+        <el-form-item
+          :label="$t('resourceMgr.ipVersion')"
+          prop="ipVersion"
+          class="w100"
+        >
+          <el-select
+            size="small"
+            v-model="createNetworkForm.ipVersion"
+            :style="{width: '100%'}"
+          >
+            <el-option
+              v-for="item in ipVersionList"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          :label="$t('resourceMgr.gatewayIP')"
+          prop="gatewayIp"
+          class="w100"
+        >
+          <el-input
+            size="small"
+            v-model="createNetworkForm.gatewayIp"
+          />
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button
+          type="primary"
+          size="small"
+          @click="confirmAction"
+        >
+          {{ $t('resourceMgr.confirm') }}
+        </el-button>
+        <el-button
+          type="primary"
+          size="small"
+          @click="cancelAction"
+        >
+          {{ $t('resourceMgr.cancel') }}
+        </el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import { appo } from '../../../tools/request.js'
+export default {
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+      dialogVisible: true,
+      ipVersionList: [
+        { label: 'ipv4', value: 'ipv4' },
+        { label: 'ipv6', value: 'ipv6' }
+      ],
+      createNetworkForm: {
+        networkName: '',
+        networkAddr: '',
+        ipVersion: '',
+        gatewayIp: '',
+        subnetName: ''
+      },
+      rules: {
+        networkName: [{ required: true, message: this.$t('resourceMgr.nameRule'), trigger: 'blur' }],
+        networkAddr: [{ required: true, message: this.$t('resourceMgr.networkAddrRule'), trigger: 'blur' }],
+        ipVersion: [{ required: true, message: this.$t('resourceMgr.ipVersionRule'), trigger: 'blur' }],
+        subnetName: [{ required: true, message: this.$t('resourceMgr.subnetNameRule'), trigger: 'blur' }]
+      },
+      language: localStorage.getItem('language')
+    }
+  },
+  methods: {
+    handleClose () {
+      this.$emit('input', false)
+      this.dialogVisible = false
+    },
+    confirmAction () {
+      let _hostIp = sessionStorage.getItem('hostIp')
+      let _params = {
+        network: {
+          name: this.createNetworkForm.networkName,
+          adminStateUp: true,
+          dnsDomain: 'my-domain.org.',
+          mtu: 1400,
+          portSecurityEnabled: true,
+          providerNetworkType: 'vlan',
+          providerPhysicalNetwork: 'network',
+          providerSegmentationId: 1,
+          qosPolicyId: null,
+          routerExternal: true,
+          segments: [
+            {
+              providerSegmentationId: 1,
+              providerPhysicalNetwork: 'abc',
+              providerNetworkType: ''
+            }
+          ],
+          shared: true,
+          vlanTransparent: true,
+          isDefault: true,
+          subnets: [{
+            name: this.createNetworkForm.subnetName,
+            ipVersion: this.createNetworkForm.ipVersion,
+            cidr: this.createNetworkForm.networkAddr,
+            gatewayIp: this.createNetworkForm.gatewayIp,
+            enableDhcp: true,
+            dnsNameservers: ['', ''],
+            allocationPools: [{
+              start: '192.168.1.5',
+              end: '192.168.2.25'
+            }],
+            ipv6AddressMode: '',
+            ipv6RaMode: ''
+          }]
+        }
+      }
+      appo.createNetwork(_hostIp, _params).then(res => {
+        this.$message.success(this.$t('resourceMgr.createNetworkSuccess'))
+        this.$emit('reloadTableData')
+        this.handleClose()
+      }).catch((error) => {
+        console.log(error)
+        this.handleClose()
+      })
+    },
+    cancelAction () {
+      this.handleClose()
+    }
+  },
+  mounted () {
+  },
+  watch: {
+    '$i18n.locale': function () {
+      this.language = localStorage.getItem('language')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.network-form{
+  .w100 {
+    width: 75%;
+    display: inline-block;
+  }
+}
+</style>
---
+++ src/pages/resource/manager/FlavorManager.vue
@@ -0,0 +1,295 @@
+<!--
+  - Copyright 2021 Huawei Technologies Co., Ltd.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+<template>
+  <div class="flavor-content">
+    <div class="search-createBtn">
+      <el-row :gutter="24">
+        <el-col
+          :span="8"
+          class="search-col"
+        >
+          <el-input
+            v-model="nameQueryVal"
+            @change="queryFlavor"
+            :placeholder="$t('resourceMgr.searchPlaceholder')"
+            class="enterinput lt"
+          >
+            <em
+              slot="suffix"
+              class="search_icon"
+              @click="queryFlavor"
+            />
+          </el-input>
+        </el-col>
+        <el-col
+          :span="12"
+          class="create-col"
+        >
+          <el-button
+            :class="language==='cn'? 'create-btn': 'create-btn-en'"
+            id="createFlavorBtn"
+            @click="createFlavor()"
+          >
+            {{ $t('resourceMgr.createFlavor') }}
+          </el-button>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="flavor-table">
+      <el-table
+        :data="currentPageData"
+        class="tableStyle"
+        ref="multipleTable"
+        v-loading="dataLoading"
+        height="400"
+      >
+        <el-table-column
+          prop="name"
+          label="Flavor Name"
+          width="165"
+          sortable
+        />
+        <el-table-column
+          prop="id"
+          label="ID"
+          width="100"
+        />
+        <el-table-column
+          prop="vcpus"
+          label="VCPUs"
+          width="90"
+        />
+        <el-table-column
+          prop="ram"
+          label="RAM(MB)"
+          width="130"
+        />
+        <el-table-column
+          prop="rootDisk"
+          label="Root Disk"
+          width="130"
+        />
+        <el-table-column
+          prop="ephemeralDisk"
+          label="Ephemeral Disk"
+          width="180"
+        />
+        <el-table-column
+          prop="isPublic"
+          label="Public"
+          width="120"
+          :formatter="formatBoolean"
+        />
+        <el-table-column
+          label="Actions"
+          width="170"
+          fixed="right"
+        >
+          <template slot-scope="scope">
+            <el-button
+              class="operations_btn"
+              id="flavorEditBtn"
+              @click.native.prevent="editFlavor(scope.row)"
+              type="text"
+              size="small"
+              :disabled="true"
+            >
+              {{ $t('resourceMgr.edit') }}
+            </el-button>
+            <el-button
+              class="operations_btn"
+              id="flavorDeleteBtn"
+              @click.native.prevent="deleteFlavor(scope.row)"
+              type="text"
+              size="small"
+            >
+              {{ $t('resourceMgr.delete') }}
+            </el-button>
+          </template>
+        </el-table-column>
+        <template slot="empty">
+          <div>
+            <img
+              src="../../../assets/images/empty.png"
+              alt=""
+              style="padding: 10px;"
+            >
+            <p>{{ $t('resourceMgr.noData') }}</p>
+          </div>
+        </template>
+      </el-table>
+      <div class="pageBar">
+        <Pagination
+          :page-sizes="[10,15,20,25]"
+          :table-data="paginationData"
+          @getCurrentPageData="getCurrentPageData"
+        />
+      </div>
+    </div>
+    <div v-if="isShowForm">
+      <FlavorForm
+        v-model="isShowForm"
+        @reloadTableData="reloadTableData"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import Pagination from '../../../components/common/Pagination.vue'
+import FlavorForm from '../form/FlavorForm.vue'
+import { appo } from '../../../tools/request.js'
+export default {
+  components: {
+    Pagination,
+    FlavorForm
+  },
+  props: {
+  },
+  data () {
+    return {
+      nameQueryVal: '',
+      paginationData: [],
+      currentPageData: [],
+      isShowForm: false,
+      dataLoading: true,
+      language: localStorage.getItem('language')
+    }
+  },
+  methods: {
+    editFlavor () {
+
+    },
+    formatBoolean (row, column, cellValue) {
+      var ret = ''
+      if (cellValue) {
+        ret = 'true'
+      } else {
+        ret = 'false'
+      }
+      return ret
+    },
+    deleteFlavor (row) {
+      this.$confirm(this.$t('resourceMgr.deleteFlavorMessage'), this.$t('resourceMgr.deleteFlavorTitle'), {
+        confirmButtonText: this.$t('common.confirm'),
+        cancelButtonText: this.$t('common.cancel'),
+        type: 'warning'
+      }).then(() => {
+        let _hostIp = sessionStorage.getItem('hostIp')
+        appo.deleteFlavorByFlavorId(_hostIp, row.id).then(res => {
+          this.$message.success(this.$t('resourceMgr.deleteSuccess'))
+          this.getTableData()
+        }).catch((error) => {
+          console.log(error)
+        })
+      }).catch(() => {
+      })
+    },
+    createFlavor () {
+      this.isShowForm = true
+    },
+    filterTableData (val, key) {
+      this.dataLoading = true
+      let _hostIp = sessionStorage.getItem('hostIp')
+      appo.queryFlavorsByMechost(_hostIp).then(res => {
+        this.paginationData = res.data.data
+        this.paginationData = this.paginationData.filter(item => {
+          let _itemVal = item[key].toLowerCase()
+          return _itemVal.indexOf(val) > -1
+        })
+        this.dataLoading = false
+      }).catch((error) => {
+        this.dataLoading = false
+        console.log(error)
+      })
+    },
+    queryFlavor () {
+      if (this.nameQueryVal && this.nameQueryVal.length > 0) {
+        this.filterTableData(this.nameQueryVal, 'name')
+      } else {
+        this.reloadTableData()
+      }
+    },
+    getCurrentPageData (data) {
+      this.currentPageData = data
+    },
+    getTableData () {
+      let _hostIp = sessionStorage.getItem('hostIp')
+      appo.queryFlavorsByMechost(_hostIp).then(res => {
+        this.paginationData = res.data.data
+        this.dataLoading = false
+      }).catch((error) => {
+        this.dataLoading = false
+        console.log(error)
+      })
+    },
+    reloadTableData () {
+      this.getTableData()
+    }
+  },
+  mounted () {
+    this.getTableData()
+  },
+  watch: {
+    '$i18n.locale': function () {
+      this.language = localStorage.getItem('language')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.flavor-content{
+  width: 1053px;
+  height: 613px;
+  padding-top: 1px;
+  border-radius: 16px;
+  box-shadow: 0px 3px 62px 6px rgba(226, 220, 247, 0.6) inset;
+  .search-createBtn{
+    .search-col{
+      margin-top: 30px;
+      margin-left: 30px;
+    }
+    .create-col{
+      text-align: center;
+      .create-btn{
+        margin-left: 486px;
+        margin-top: 30px;
+        height: 40px;
+        color: #fff;
+        font-size: 20px !important;
+        border-radius: 10px;
+        padding: 0 35px;
+        background-image: linear-gradient(127deg, #4444d0, #6724cb);
+        border: none;
+      }
+      .create-btn-en{
+        margin-left: 445px;
+        margin-top: 30px;
+        height: 40px;
+        color: #fff;
+        font-size: 20px !important;
+        border-radius: 10px;
+        padding: 0 35px;
+        background-image: linear-gradient(127deg, #4444d0, #6724cb);
+        border: none;
+      }
+    }
+  }
+  .flavor-table{
+    width: 1000px;
+    margin: 30px auto;
+  }
+}
+</style>
