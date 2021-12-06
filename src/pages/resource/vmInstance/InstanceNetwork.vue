c80621d4fc4a4f5c0a58479ed1fe96660a9dd399
zhaolongfei_123 <951084082@qq.com>
2021-12-06 16:20:20 +0800

    I4G4W1【mecm-fe】MECM中心侧支持轻量级云管功能-同步mecm

---
+++ src/pages/resource/vmInstance/InstanceNetwork.vue
@@ -0,0 +1,151 @@
+<!--
+  - Copyright 2021 Huawei Technologies Co., Ltd.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+<template>
+  <div>
+    <div>
+      <el-table
+        :data="currentPageData"
+        class="tableStyle"
+        @selection-change="selectionLineChangeHandle"
+        ref="multipleTable"
+        height="260"
+      >
+        <el-table-column
+          type="selection"
+          width="50"
+        />
+        <el-table-column
+          prop="name"
+          label="Network"
+          width="150px"
+          sortable
+        />
+        <el-table-column
+          :label="$t('resourceMgr.fixedIp')"
+          width="180px"
+        >
+          <template slot-scope="scope">
+            <el-input
+              type="text"
+              size="small"
+              placeholder="enter fixedIp"
+              v-model="scope.row.fixedIp"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="shared"
+          :label="$t('resourceMgr.shared')"
+          width="90px"
+          :formatter="formatBoolean"
+        />
+        <el-table-column
+          prop="adminStatus"
+          :label="$t('resourceMgr.adminStatus')"
+          width="200px"
+          :formatter="formatBoolean"
+        />
+        <el-table-column
+          prop="status"
+          :label="$t('resourceMgr.status')"
+        />
+        <template slot="empty">
+          <div>
+            <img
+              src="../../../assets/images/empty.png"
+              alt=""
+              style="padding: 10px;"
+            >
+            <p>{{ $t('resourceMgr.noData') }}</p>
+          </div>
+        </template>
+      </el-table>
+      <div class="pageBar">
+        <Pagination
+          :page-sizes="[10,15,20,25]"
+          :table-data="paginationData"
+          @getCurrentPageData="getCurrentPageData"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Pagination from '../../../components/common/Pagination.vue'
+import { appo } from '../../../tools/request.js'
+export default {
+  components: {
+    Pagination
+  },
+  props: {
+  },
+  data () {
+    return {
+      dialogVisible: true,
+      currentPageData: [],
+      paginationData: [],
+      selectNetwork: {
+        step: 'stepNetwork',
+        networks: [],
+        availabilityZone: ''
+      }
+    }
+  },
+  methods: {
+    getCurrentPageData (data) {
+      this.currentPageData = data
+    },
+    formatBoolean (row, column, cellValue) {
+      var ret = ''
+      if (cellValue) {
+        ret = 'true'
+      } else {
+        ret = 'false'
+      }
+      return ret
+    },
+    selectionLineChangeHandle (val) {
+      val.forEach(item => {
+        let _tempNetwork = {
+          network: item.id,
+          fixedIp: item.fixedIp
+        }
+        this.selectNetwork.networks.push(_tempNetwork)
+        this.selectNetwork.availabilityZone = item.availabilityZones[0]
+      })
+    },
+    // receive msg from parent component
+    parentMsg: function (active) {
+      this.$emit('getStepData', this.selectNetwork)
+    },
+    getTableData () {
+      let _hostIp = sessionStorage.getItem('hostIp')
+      appo.queryNetworksByMechost(_hostIp).then(res => {
+        this.paginationData = res.data.data
+        this.dataLoading = false
+      }).catch((error) => {
+        this.dataLoading = false
+        console.log(error)
+      })
+    }
+  },
+  mounted () {
+    this.getTableData()
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
